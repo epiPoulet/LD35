29d72af0e6fb6b16fb98b155430f594c36f18666
gandoulf <enzo.resse@epitech.eu>
2016-04-18 00:54:38 +0100

    Pillar asset

---
+++ LD35/Assets/Script/AEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AEvent : MonoBehaviour {
+
+	public GameObject item;
+
+	protected GameObject[] map;
+	protected float[] spectrum;
+	protected float currentEvent = 0;
+
+	public abstract void update ();
+
+	public void sendData(ref GameObject[] _map, ref float[] _spectrum)
+	{
+		map = _map;
+		spectrum = _spectrum;
+	}
+}
---
+++ LD35/Assets/Script/LinePop.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinePop : AEvent {
+
+	public float neededPower;
+	public int number;
+
+	public float popTimeMin;
+	public float popTimeMax;
+
+	private float popTime = 1;
+
+	public override void update () {
+		currentEvent += Time.deltaTime;
+		if (currentEvent >= popTime && checkSoundPower()) {
+			int randItem = Random.Range (0, map.Length);
+			for (int i = 0; randItem + i < map.Length && i < 4; ++i) {
+				Instantiate (item, new Vector3 (map [randItem + i].transform.position.x, 10, 0), item.transform.rotation);
+			}
+			popTime = Random.Range (popTimeMin, popTimeMax);
+			currentEvent = 0;
+		}
+	}
+
+	private bool checkSoundPower() {
+		int nbOverPower = 0;
+
+		for (int i = 0; i < spectrum.Length; ++i)
+			if (spectrum [i] > neededPower)
+				++nbOverPower;
+		if (nbOverPower >= number) {
+			return true;
+		}
+		return false;
+	}
+}
